31e4ad5e6bacf6dc0419a4e291199f74d2c6c7b3
Charity_Foundation <yurijdyakiv@gmail.com>
2018-10-21 20:40:05 +0300

    Lab4

--- app/src/main/java/com/example/urecdakiv/my_first_application/SignUpActivity.java
+++ app/src/main/java/com/example/urecdakiv/my_first_application/SignUpActivity.java
@@ -1,16 +1,19 @@
 package com.example.urecdakiv.my_first_application;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity extends AppCompatActivity {
 
   private EditText firstName, lastName, email;
   private EditText phone, password, confirmPassword;
-  private Button submitButton;
+  private Button submitButton, nextLabButton;
   private String emailText, phoneText, passwordText, confPassText;
   private String firstNameText, lastNameText;
 
@@ -22,26 +25,23 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     initFields();
   }
 
-
-  @Override
-  public void onClick(View view) {
-
-    emailText = email.getText().toString();
-    passwordText = password.getText().toString();
-    firstNameText = firstName.getText().toString();
-    lastNameText = lastName.getText().toString();
-    passwordText = password.getText().toString();
-    confPassText = confirmPassword.getText().toString();
-    phoneText = phone.getText().toString();
-    checkFields();
+  private void saveInfo() {
+    SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+    editor.putString("name", firstName.getText().toString());
+    editor.putString("surname", lastName.getText().toString());
+    editor.putString("email", email.getText().toString());
+    editor.putString("phone", phone.getText().toString());
+    editor.apply();
   }
+
 
   public void checkFields() {
     if (!Validations.isValidPassword(passwordText)
-        || !Validations.isValidPassword(confPassText)
-        || !passwordText.equals(confPassText)
-        || !Validations.isValidPhoneNumber(phoneText)
-        || !Validations.isValidEmail(emailText)) {
+            || !Validations.isValidPassword(confPassText)
+            || !passwordText.equals(confPassText)
+            || !Validations.isValidPhoneNumber(phoneText)
+            || !Validations.isValidEmail(emailText)) {
 
       if (!Validations.isValidFirstName(firstNameText)) {
         firstName.setError(getText(R.string.badFirtsname));
@@ -65,6 +65,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         confirmPassword.setError(getText(R.string.badPasswordConfirmation));
       }
     }
+    saveInfo();
   }
 
   public void initFields() {
@@ -75,6 +76,28 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     password = findViewById(R.id.passwordEt);
     confirmPassword = findViewById(R.id.passwordConfirmEt);
     submitButton = findViewById(R.id.submitButton);
-    submitButton.setOnClickListener(this);
+    nextLabButton = findViewById(R.id.btn_change_act);
+
+    submitButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        emailText = email.getText().toString();
+        passwordText = password.getText().toString();
+        firstNameText = firstName.getText().toString();
+        lastNameText = lastName.getText().toString();
+        passwordText = password.getText().toString();
+        confPassText = confirmPassword.getText().toString();
+        phoneText = phone.getText().toString();
+        checkFields();
+      }
+    });
+
+    nextLabButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent activityChangeIntent = new Intent(SignUpActivity.this, SignUpDataActivity.class);
+        SignUpActivity.this.startActivity(activityChangeIntent);
+      }
+    });
   }
 }
